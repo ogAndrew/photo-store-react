28c6573bc7377521a23a99991e07293eab0cebda
Andrew Talle <talle.andrew@gmail.com>
2020-10-25 18:57:37 -0700

    hydrate context with photos api

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 const Context = React.createContext();
 
+const url = `https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json`
+
 function ContextProvider({children}) {
+    const [allPhotos, setAllPhotos] = useState([]);
+    
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
+    }, [])
+
+    console.log(allPhotos);
+
     return (
         <div>
-            <Context.Provider value={""}>
+            <Context.Provider value={{allPhotos}}>
                 {children}
             </Context.Provider>
         </div>
