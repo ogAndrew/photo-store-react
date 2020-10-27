de5afa35fa00f05afeb2ee65a9facf67978f28d3
Andrew Talle <talle.andrew@gmail.com>
2020-10-26 20:07:10 -0700

    add state and ui for hover icons

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Image({className, img}) {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const heartIcon = isHovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = isHovered && <i className="ri-add-circle-line cart"></i> 
+
     return (
-        <div className={`${className} image-container`}>
+        <div 
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            className={`${className} image-container`}
+        >
             <img src={img.url} className="image-grid" alt={img.id} />
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
